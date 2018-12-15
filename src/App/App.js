ca973dedacd1e7a750293a3f50c341d3a7f61429
Timothy Harley <timothy.d.harley@gmail.com>
2018-12-15 10:24:05 -0600

    get request worked

--- src/App/App.js
+++ src/App/App.js
@@ -6,16 +6,24 @@ import connection from '../helpers/data/connection';
 import Auth from '../components/Auth/Auth';
 import Listings from '../components/Listings/Listings';
 import MyNavbar from '../components/MyNavbar/MyNavbar';
+import listingRequests from '../helpers/data/listingRequests';
 import './App.scss';
 import authRequests from '../helpers/data/authRequests';
 
 class App extends Component {
   state = {
     authed: false,
+    listings: [],
   }
 
   componentDidMount() {
     connection();
+
+    listingRequests.getRequest()
+      .then((listings) => {
+        this.setState({ listings }); // this is shorthand notation for listings: listings
+      });
+
     this.removeListener = firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.setState({
@@ -57,7 +65,7 @@ class App extends Component {
       <div className="App">
         <MyNavbar isAuthed={this.state.authed} logoutClickEvent={logoutClickEvent} />
         <div className="row">
-          <Listings />
+          <Listings listings={this.state.listings} />
         </div>
         <div className="row">
         </div>
